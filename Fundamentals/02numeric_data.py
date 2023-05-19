6e5e14e76270797f3ba77360ad83148fd0b7dde1
manu <emanuelletorres982@gmail.com>
2023-05-18 22:47:53 -0300

    integers and floats in py

---
+++ Fundamentals/02numeric_data.py
@@ -0,0 +1,33 @@
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+# Absolute value
+print(abs(-2))
+
+# round(_,_)
+print(round(3.75)) # 4
+print(round(3.75, 1)) # 3.8
+
+#
+num_1 = '100'
+num_2 = '200'
+print(num_1 + num_2) # 100200
+
+# Casting from string to integer
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1 + num_2) # 300

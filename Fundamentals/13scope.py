ff422a8347039fd261dd21362e00509c1b7307a9
manu <emanuelletorres982@gmail.com>
2023-05-20 21:12:55 -0300

    variable scope in py

---
+++ Fundamentals/13scope.py
@@ -0,0 +1,28 @@
+'''
+LEGB - the order of scope checked by python
+Local, Enclosing, Global, Built-in methods
+'''
+
+# Overwriting a global variable
+""" x = 'global x'
+def test():
+    global x # tells python we're working with the global x now
+    x = 'local x'
+    print(x)
+
+test()
+print(x) """
+
+# Working with enclosing functions
+def outer():
+    x = 'outer x'
+
+    def inner():
+        nonlocal x # overwriting the nonlocal x variables
+        x = 'inner x'
+        print(x)
+
+    inner()
+    print(x)
+
+outer()
